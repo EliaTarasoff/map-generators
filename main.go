412ac4fe5b6bda2969c3f343e117c62b621cbae8
EliaTarasoff <elia.tarasoff@gmail.com>
2023-07-18 18:41:14 -0600

    wip: sketch out some rooms in comments

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+const SubBlocksPerBlock = 2
+
+func main() {
+
+}
+
+/*
+e.g. two rooms
+xxx
+x x    xxx
+x |====| x
+xxx    x x
+       x x
+       xxx
+
+e.g. one big room and two small ones
+xxxxxx xxx
+x    |=| x
+x    x xxx
+x    x  xxxx
+x    |==|  x
+x    x  xxxx
+xxxxxx
+
+e.g. two rooms sharing a wall (part-way)
+xxxx
+x  x
+x  |
+xxxxxx
+x    |
+xxxxxx
+*/
