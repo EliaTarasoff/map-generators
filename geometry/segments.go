ede848a37fe44b48b2c40bfb29ce91ae68831441
EliaTarasoff <elia.tarasoff@gmail.com>
2023-08-07 22:19:42 -0600

    wip: getHighest(a, b Segment), simpler than getHighest(s []Segment)

--- geometry/segments.go
+++ geometry/segments.go
@@ -23,3 +23,49 @@ func GetHighestValueSegments(segments []Segment) []Segment {
 	for _, segment := range sorted {
 	}
 }
+
+func getHighestSegments(a, b Segment) []Segment {
+	if a.Right < b.Left {
+		return []Segment{a, b}
+	}
+	if b.Right < a.Left {
+		return []Segment{b, a}
+	}
+
+	left := Segment{}
+	right := Segment{}
+
+	if a.Left < b.Left {
+		left = copySegment(a)
+	} else {
+		left = copySegment(b)
+	}
+
+	if a.Right > b.Right {
+		right = copySegment(a)
+	} else {
+		right = copySegment(b)
+	}
+
+	if left.Height > right.Height {
+		right.Left += 1
+	} else {
+		left.Right -= 1
+	}
+
+	if left.Left == left.Right {
+		return []Segment{right}
+	}
+	if right.Left == right.Right {
+		return []Segment{left}
+	}
+	return []Segment{left, right}
+}
+
+func copySegment(a Segment) Segment {
+	return Segment{
+		Left:   a.Left,
+		Right:  a.Right,
+		Height: a.Height,
+	}
+}
