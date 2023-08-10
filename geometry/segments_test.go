e90f14e6e17171c45da4d509d297f0f4a7177c37
EliaTarasoff <elia.tarasoff@gmail.com>
2023-08-10 12:51:53 -0600

    test: named subtests

--- geometry/segments_test.go
+++ geometry/segments_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestGetHighestValueSegments(t *testing.T) {
-	failMatch := func(got, expected []Segment) {
+	failMatch := func(got, expected []Segment, t *testing.T) {
 		t.Error(spew.Sprintf("got high-segments %v, expected %v", got, expected))
 	}
 	segmentsMatch := func(ins, expecteds []Segment) bool {
@@ -24,97 +24,96 @@ func TestGetHighestValueSegments(t *testing.T) {
 		}
 		return true
 	}
-
-	var in []Segment
-	var expected []Segment
-	highest := GetHighestValueSegments(in)
-	if !segmentsMatch(highest, expected) {
-		failMatch(highest, expected)
+	getAndCheck := func(ins, expecteds []Segment, t *testing.T) {
+		highs := GetHighestValueSegments(ins)
+		if !segmentsMatch(highs, expecteds) {
+			failMatch(highs, expecteds, t)
+		}
 	}
 
-	in = []Segment{
-		{
-			Height: 1,
-			Left:   2,
-			Right:  3,
-		},
-	}
-	expected = in
-	highest = GetHighestValueSegments(in)
-	if !segmentsMatch(highest, expected) {
-		failMatch(highest, expected)
-	}
+	t.Run("zero segments", func(t *testing.T) {
+		getAndCheck(nil, nil, t)
+	})
 
-	in = []Segment{
-		{
-			Height: 7,
-			Left:   5,
-			Right:  10,
-		},
-		{
-			Height: 3,
-			Left:   0,
-			Right:  8,
-		},
-	}
-	expected = []Segment{
-		{
-			Height: 3,
-			Left:   0,
-			Right:  4,
-		},
-		{
-			Height: 7,
-			Left:   5,
-			Right:  10,
-		},
-	}
-	highest = GetHighestValueSegments(in)
-	if !segmentsMatch(highest, expected) {
-		failMatch(highest, expected)
-	}
+	t.Run("one segment", func(t *testing.T) {
+		in := []Segment{
+			{
+				Height: 1,
+				Left:   2,
+				Right:  3,
+			},
+		}
+		getAndCheck(in, in, t)
+	})
 
-	in = []Segment{
-		{
-			Height: 7,
-			Left:   5,
-			Right:  10,
-		},
-		{
-			Height: 3,
-			Left:   0,
-			Right:  8,
-		},
-		{
-			Height: 2,
-			Left:   -1,
-			Right:  12,
-		},
-	}
-	expected = []Segment{
-		{
-			Height: 2,
-			Left:   -1,
-			Right:  -1,
-		},
-		{
-			Height: 3,
-			Left:   0,
-			Right:  4,
-		},
-		{
-			Height: 7,
-			Left:   5,
-			Right:  10,
-		},
-		{
-			Height: 2,
-			Left:   11,
-			Right:  12,
-		},
-	}
-	highest = GetHighestValueSegments(in)
-	if !segmentsMatch(highest, expected) {
-		failMatch(highest, expected)
-	}
+	t.Run("two overlapping segments", func(t *testing.T) {
+		in := []Segment{
+			{
+				Height: 7,
+				Left:   5,
+				Right:  10,
+			},
+			{
+				Height: 3,
+				Left:   0,
+				Right:  8,
+			},
+		}
+		expected := []Segment{
+			{
+				Height: 3,
+				Left:   0,
+				Right:  4,
+			},
+			{
+				Height: 7,
+				Left:   5,
+				Right:  10,
+			},
+		}
+		getAndCheck(in, expected, t)
+	})
+
+	t.Run("two overlapping segments", func(t *testing.T) {
+		in := []Segment{
+			{
+				Height: 7,
+				Left:   5,
+				Right:  10,
+			},
+			{
+				Height: 3,
+				Left:   0,
+				Right:  8,
+			},
+			{
+				Height: 2,
+				Left:   -1,
+				Right:  12,
+			},
+		}
+		expected := []Segment{
+			{
+				Height: 2,
+				Left:   -1,
+				Right:  -1,
+			},
+			{
+				Height: 3,
+				Left:   0,
+				Right:  4,
+			},
+			{
+				Height: 7,
+				Left:   5,
+				Right:  10,
+			},
+			{
+				Height: 2,
+				Left:   11,
+				Right:  12,
+			},
+		}
+		getAndCheck(in, expected, t)
+	})
 }
